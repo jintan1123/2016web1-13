99e27a77d78d23223248f00d1bf8f2497c9391d2
jintan1123 <g1527040@st.t-kougei.ac.jp>
2016-07-04 17:59:00 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,10 +1,11 @@
 var separate_time = function(time){
   var sec = time.getSeconds();
   var min = time.getMinutes();
-  var hours = time.getDate();
+  var hours = time.getHours();
+  var days = time.getDate();
   var month = time.getMonth();
   var year = time.getFullYear();
-  return [sec,min,hours,month,year];
+  return [sec,min,hours,days,month,year];
 }
 
 var now =new Date();
